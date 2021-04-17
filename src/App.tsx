e5bbb97ab4e2af7b8854ad5ac6d1491deae30c7d
Marid <maridshikhkerimov@gmail.com>
2021-04-17 22:57:53 +0300

    add yarn uuid

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ function App() {
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
-        {id: 4, title: "ReactJS", isDone: false},
+        {id: 4, title: "Rest API", isDone: false},
+        {id: 5, title: "GraphQL", isDone: false}
     ]);
 
     function removeTask(id: number) {
